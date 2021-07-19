8ead8ec88998e54ca0227e756a7b78ce00678da2
By-Biska <varakinkirill3@gmail.com>
2021-07-19 09:48:10 +0300

    rightside-menu

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+$((function(){$(".header__btn").on("click",(function(){$(".rightside-menu").removeClass("rightside-menu--close")})),$(".rightside-menu__close").on("click",(function(){$(".rightside-menu").addClass("rightside-menu--close")}))}));
