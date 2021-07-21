4a6834be878777859d4201bd14978d8f61a0d6f2
By-Biska <varakinkirill3@gmail.com>
2021-07-21 10:41:41 +0300

    homw-2

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-$((function(){$(".header__btn").on("click",(function(){$(".rightside-menu").removeClass("rightside-menu--close")})),$(".rightside-menu__close").on("click",(function(){$(".rightside-menu").addClass("rightside-menu--close")}))}));
+$((function(){$(".header__btn").on("click",(function(){$(".rightside-menu").removeClass("rightside-menu--close")})),$(".rightside-menu__close").on("click",(function(){$(".rightside-menu").addClass("rightside-menu--close")})),$(".top__slider").slick({dots:!0,arrows:!1})}));
